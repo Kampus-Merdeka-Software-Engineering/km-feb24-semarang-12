27cb9fcc7a625e903417b2669af837ec30402c3e
halvinopanji <halvinopanji9i@gmail.com>
2024-05-21 21:13:05 +0700

    add data chart

--- chart1.js
+++ chart1.js
@@ -25,11 +25,11 @@ function createChart(data, type){
 chart1 = new Chart(ctx, {
   type: type,
   data: {
-    labels: 
-    data.map (row => row.BOROUGH).slice(0, limit),
+    labels: data.map (row => row.BOROUGH).slice(0, limit),
     datasets: [{
       label: 'BOROUGH',
-      data: data.map [12, 19, 3, 5, 2, 3],
+      data:  [12, 19, 3, 5, 2, 3],
+
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderColor: 'rgba(75, 192, 192, 1)',
       borderWidth: 1
