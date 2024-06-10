24d16054d18bed50f9a9033c32fe09963cca3bc2
halvinopanji <halvinopanji9i@gmail.com>
2024-06-10 17:01:28 +0700

    responsive-dahsboard

--- js/data-table.js
+++ js/data-table.js
@@ -1,3 +1,13 @@
+let navbar = document.querySelector('.header .navbar')
+
+            document.querySelector('#menu').onclick = () =>{
+            navbar.classList.add('active');
+            }
+
+            document.querySelector('#close').onclick = () =>{
+            navbar.classList.remove('active');
+            }
+
 $(document).ready(function() {
   // Fetch the JSON data
   $.getJSON('data.json', function(data) {
